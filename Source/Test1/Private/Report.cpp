a9e1176d68b5af68c812f3a6b1bc2f04ff700cb4
DrMole1 <32479979+DrMole1@users.noreply.github.com>
2020-03-19 10:26:27 +0100

    Commit 2

---
+++ Source/Test1/Private/Report.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Report.h"
+
+// Sets default values
+AReport::AReport()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AReport::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AReport::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
